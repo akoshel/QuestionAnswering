1f879538c386665fb1eac54f2325a5736f09209d
akoshel <johndoe@example.com>
2021-08-16 18:45:39 +0300

    train fixed

--- question_answering/utils/utils.py
+++ question_answering/utils/utils.py
@@ -10,14 +10,10 @@ def train_epoch(model: nn.Module, iterator: DataLoader, criterion, optimizer, de
     logger.info("train epoch started")
     for i, batch in enumerate(iterator):
         features, attention_mask, start_token, end_token = batch
-        features.to(device)
-        attention_mask.to(device)
-        start_token.to(device)
-        end_token.to(device)
-        start_logits, end_logits = model(features, attention_mask)
-        start_loss = criterion(start_logits, start_token)
+        start_logits, end_logits = model(features.to(device), attention_mask.to(device))
+        start_loss = criterion(start_logits, start_token.to(device))
         # start_loss.backward()
-        end_loss = criterion(end_logits, end_token)
+        end_loss = criterion(end_logits, end_token.to(device))
         # end_loss.backward()
         total_loss = (start_loss + end_loss) / 2
         total_loss.backward()
